2004bda190d278107f9518374a16bbc0875dc48f
Gibdos <simon.yousefi@protonmail.com>
2022-02-25 21:46:40 +0100

    +10% opacity change per mouseover challenge done

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const container = document.querySelector(".container");
 const tileNumber = document.querySelector("#tile-number");
 const btnStartGame = document.querySelector("#start-game");
 newDiv.className = "playfield";
+newDiv.setAttribute("style", "background-color: rgba(0, 0, 0, 0.0);");
 
 for (i = 0; i < 100; i++) {
 	container.appendChild(newDiv.cloneNode(true));
@@ -12,7 +13,11 @@ const etcher = document.querySelectorAll(".playfield");
 
 etcher.forEach((e) => {
 	e.addEventListener("mouseover", () => {
-		e.style.backgroundColor = "black";
+		let currentOpacity = parseFloat(e.getAttribute("style").slice(-5, -2));
+		console.log(currentOpacity);
+		let newOpacity = currentOpacity + 0.1;
+		console.log(newOpacity);
+		e.style.backgroundColor = "rgba(0, 0, 0, " + newOpacity + ")";
 	});
 });
 
@@ -31,7 +36,11 @@ function startGame() {
 		const etcher = document.querySelectorAll(".playfield");
 		etcher.forEach((e) => {
 			e.addEventListener("mouseover", () => {
-				e.style.backgroundColor = "black";
+				let currentOpacity = parseFloat(e.getAttribute("style").slice(-5, -2));
+				console.log(currentOpacity);
+				let newOpacity = currentOpacity + 0.1;
+				console.log(newOpacity);
+				e.style.backgroundColor = "rgba(0, 0, 0, " + newOpacity + ")";
 			});
 		});
 	}
